fcbd43de30c44c09f72cdef37a7bc83a8d349003
tomoya baba <tomoya.bb17931837@gmail.com>
2025-02-06 15:49:24 +0900

    tukareta

---
+++ ex03/Fixed.cpp
@@ -0,0 +1,173 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed(): val(0)
+{
+	// std::cout << "Default Constructor called\n";
+}
+
+Fixed::Fixed(const int input)
+{
+	// std::cout << "Int Constructor called\n";
+	this->val = input << this->eight_val;
+}
+
+Fixed::Fixed(const float input)
+{
+	// std::cout << "Float Constructor called\n";
+	this->val = roundf(input * 256);
+}
+
+Fixed::~Fixed()
+{
+	// std::cout << "Deconstructor called\n";
+}
+
+Fixed::Fixed(const Fixed &copy)
+{
+	// std::cout << "Copy constructor called\n";
+	*this = copy;
+}
+
+Fixed &Fixed::operator=(const Fixed &src)
+{
+	// std::cout << "Copy assignation operator called\n";
+	if (this != &src)
+		this->val = src.getRawBits();
+	return (*this);
+}
+
+int	Fixed::getRawBits(void)const
+{
+	// std::cout << "getRawBits member function called\n";
+	return (this->val);
+}
+
+void	Fixed::setRawBits(int const raw)
+{
+	// std::cout << "setRawBits member function called\n";
+	this->val = raw;
+}
+
+float	Fixed::toFloat(void)const
+{
+	return ((float)this->val/256);
+}
+
+int	Fixed::toInt(void)const
+{
+	return (this->val >> this->eight_val);
+}
+
+bool	Fixed::operator>(Fixed fixed) const
+{
+	return (this->toFloat() > fixed.toFloat());
+}
+
+bool	Fixed::operator<(Fixed fixed) const
+{
+	return (this->toFloat() < fixed.toFloat());
+}
+
+bool	Fixed::operator>=(Fixed fixed) const
+{
+	return (this->toFloat() >= fixed.toFloat());
+}
+
+bool	Fixed::operator<=(Fixed fixed) const
+{
+	return (this->toFloat() <= fixed.toFloat());
+}
+
+bool	Fixed::operator==(Fixed fixed) const
+{
+	return (this->toFloat() == fixed.toFloat());
+}
+
+bool	Fixed::operator!=(Fixed fixed) const
+{
+	return (this->toFloat() != fixed.toFloat());
+}
+
+float	Fixed::operator+(Fixed fixed) const
+{
+	return (this->toFloat() + fixed.toFloat());
+}
+
+float	Fixed::operator-(Fixed fixed) const
+{
+	return (this->toFloat() - fixed.toFloat());
+}
+
+float	Fixed::operator*(Fixed fixed) const
+{
+	return (this->toFloat() * fixed.toFloat());
+}
+
+float	Fixed::operator/(Fixed fixed) const
+{
+	return (this->toFloat() / fixed.toFloat());
+}
+
+Fixed	Fixed::operator++()
+{
+	this->val++;
+	return (*this);
+}
+
+Fixed	Fixed::operator--()
+{
+	this->val--;
+	return (*this);
+}
+
+Fixed	Fixed::operator++(int)
+{
+	Fixed tmp = *this;
+	this->val++;
+	return (tmp);
+}
+
+Fixed	Fixed::operator--(int)
+{
+	Fixed tmp = *this;
+	this->val--;
+	return (tmp);
+}
+
+Fixed	&Fixed::min(Fixed &a, Fixed &b)
+{
+	if (a.toFloat() <= b.toFloat())
+		return (a);
+	else
+		return (b);
+}
+
+const Fixed	&Fixed::min(const Fixed &a, const Fixed &b)
+{
+	if (a.toFloat() <= b.toFloat())
+		return (a);
+	else
+		return (b);
+}
+
+Fixed	&Fixed::max(Fixed &a, Fixed &b)
+{
+	if (a.toFloat() >= b.toFloat())
+		return (a);
+	else
+		return (b);
+}
+
+const Fixed	&Fixed::max(const Fixed &a, const Fixed &b)
+{
+	if (a.toFloat() >= b.toFloat())
+		return (a);
+	else
+		return (b);
+}
+
+std::ostream	&operator<<(std::ostream &res, Fixed const &fixed)
+{
+	res << fixed.toFloat();
+	return (res);
+}
